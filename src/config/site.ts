259256cb2032aaa9339cb412d482d2680d0036b1
jhosuapp <jhosuapvll@gmail.com>
2026-07-14 12:17:57 -0500

    fix: bot number

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,4 +5,4 @@ export const ADMIN_NOTIFICATION_EMAIL = "joshua@150porciento.com";
 
 // Número de WhatsApp del negocio (formato internacional sin "+").
 export const WHATSAPP_NUMBER = "573204117807";
-export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
+export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=Hola%2C%20quiero%20conocer%20cómo%20Humanika%20puede%20ayudar%20a%20mi%20organización%20con%20inteligencia%20artificial.`;
